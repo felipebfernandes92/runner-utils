c54f43a164ebd98f3e14cdb7493a48ac5cd335c0
Felipe Fernandes <fernandesfelipeb@gmail.com>
2019-05-27 22:12:41 -0300

    Integração com atividade do garmin

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,65 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <ul>
+      <li v-for="track in tracks" :key="track">
+        {{ track.tempo }} - {{ metros(track.distancia) }}
+      </li>
+    </ul>
   </div>
 </template>
+
+<script>
+import { parse } from 'path';
+let convert = require('xml-js');
+import axios from 'axios';
+import moment from 'moment';
+
+export default {
+  data() {
+    return {
+      dados: null,
+      xml: null,
+      tracks: null
+    }
+  },
+
+  methods: {
+    metros(value) {
+      return parseFloat(value).toFixed(2) + ' metros';
+    },
+
+    getAtividades() {
+      let dados = [];
+      let inicioAtividade = this.dados.elements[0].elements[0].elements[0].elements[0].elements[0].text;
+      
+      if (this.dados) {
+        for (let element of this.dados.elements[0].elements[0].elements[0].elements[1].elements[6].elements) {
+          let horaTrack = element.elements[0].elements[0].text;
+          let tempoPercorridoTrack = moment(horaTrack).diff(
+            moment(inicioAtividade), 'seconds'
+          );
+
+          dados.push({
+            'tempo': tempoPercorridoTrack,
+            'distancia': element.elements[3].elements[0].text
+          });
+        };
+
+        this.tracks = dados;
+      }
+    }
+  },
+
+  mounted() {
+    axios.get('http://localhost:8080/garmin.xml')
+        .then(response => {
+            this.xml = response.data;
+            let result = convert.xml2json(this.xml, null);
+            this.dados = JSON.parse(result);
+            this.getAtividades();
+        }
+    )
+  }
+}
+</script>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,74 @@
 <template>
   <div class="home">
-    {{ count }}
+    <!-- {{ count }}
     <p>Dobro: {{ dobroAdicionados }}</p>
     <button @click="adicionar">Adicionar</button>
-    <button @click="remover">Remover</button>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="remover">Remover</button> -->  
+  <v-form>
+    <v-container>
+      <v-layout>
+        <v-flex xs12 md12>
+          <v-text-field
+            v-model="distancia"
+            label="Distância (km)"
+            required
+          ></v-text-field>
+        </v-flex>
+
+        <v-flex xs12 md12>
+          <v-text-field
+            v-model="pace"
+            label="Pace"
+            required
+          ></v-text-field>
+        </v-flex>
+        <v-flex xs12 md12>
+          <v-text-field
+            v-model="tempoTotal"
+            label="Tempo total"
+            required
+          ></v-text-field>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-form>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+//import HelloWorld from '@/components/HelloWorld.vue'
 import { mapState, mapGetters, mapMutations } from 'vuex';
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  // components: {
+  //   HelloWorld
+  // },
+  data() {
+    return {
+      distancia: null,
+      pace: null
+    }
   },
   computed: {
+    tempoTotal() {
+      let minutosSegundos = 0;
+
+      if (this.pace) {
+        minutosSegundos = this.pace.split(':');
+        let somaSegundosPace = (minutosSegundos[1] * this.distancia) / 60;
+        let total = (this.distancia * minutosSegundos[0]) + somaSegundosPace;
+
+        let segundos = ((total % 1) * 60) / 100;
+
+        return (Math.floor(total) + segundos).toFixed(2);
+      }
+
+      return 0;
+    },
+
     ...mapState([
       'count'
     ]),
